6fdfe6cab04ae391b90f77a87e1d924784ad35a6
cryshado <mirmiclub@gmail.com>
2022-03-30 01:41:39 +0300

    change imports

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -1,5 +1,5 @@
 import { MsgTemplate } from './msg-template'
-import { WalletV3Contract } from './wallets/wallet'
+import { WalletV3Contract } from './wallets/wallet-v3'
 import { HighloadWalletV2Contract } from './wallets/highload-wallet-v2'
 
 export { MsgTemplate, WalletV3Contract, HighloadWalletV2Contract }
